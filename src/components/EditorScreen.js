ba6148dad0acb214546546d0274ed74f6844d99c
Yosef-Perelman <yosefper@gmail.com>
2024-09-09 17:57:55 +0300

    Added socket.io.

--- src/components/EditorScreen.js
+++ src/components/EditorScreen.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Editor from '@monaco-editor/react';
+import io from 'socket.io-client';
+import '../App.css'
+
+const socket = io('http://localhost:5000');
 
 const EditorScreen = () => {
-  return <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" />;
+
+  const [content, setContent] = useState('');
+
+  useEffect(() => { socket.on('updateContent', (updatedContent) => {
+            console.log('Received content from server:', updatedContent);
+            setContent(updatedContent);
+        });
+
+        return () => {
+          socket.off('updateContent');
+      };
+  },[]);
+
+  const handleEdit = (updatedContent) => { 
+    setContent(updatedContent);
+    console.log('Sending updated content to server:', updatedContent);
+    socket.emit('edit', updatedContent);
+  };
+
+  return (
+    <>
+      <div className="editor-block"> 
+      <Editor value={content} onChange={handleEdit} theme="vs-dark" defaultLanguage="javascript"/>;
+      </div>
+    </>
+    );
   }
 
 export default EditorScreen;
