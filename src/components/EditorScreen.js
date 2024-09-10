600d68b326ebd5a081934295f1f6dc919cce5774
Yosef-Perelman <yosefper@gmail.com>
2024-09-10 18:32:37 +0300

    Added room and counting the number of users in the room.

--- src/components/EditorScreen.js
+++ src/components/EditorScreen.js
@@ -4,24 +4,58 @@ import Editor from '@monaco-editor/react';
 import io from 'socket.io-client';
 import '../App.css'
 
-const socket = io('http://localhost:5000');
+let socket = null;
 
 const EditorScreen = () => {
 
   const [content, setContent] = useState('');
-  const { codeBlockName } = useParams();
+  const [students, setStudents ] = useState(0);
+  const { codeBlockName } = useParams()
 
   useEffect(() => { 
+
+        socket = io('http://localhost:5000');
+        console.log('Connecting to server');
+
+        socket.emit('join', codeBlockName);
+
+        socket.on('roomInfo', ({ roomSize }) => {
+          console.log(`Room info received. Users in room: ${roomSize}`);
+          setStudents(roomSize);
+        });
+
+        socket.on('userJoined', ({ socketId, roomSize }) => {
+          console.log(`${socketId} joined the room. Total users: ${roomSize}`);
+          setStudents(roomSize);
+        });
+
+        socket.on('userLeft', ({ socketId, roomSize }) => {
+          console.log(`${socketId} left the room. Total users: ${roomSize}`);
+          setStudents(roomSize);
+        });
 
         socket.on('updateContent', (updatedContent) => {
             console.log('Received content from server:', updatedContent);
             setContent(updatedContent);
         });
 
+      //   socket.on('someonejoined', (userId, numStudentsInTheRoom) => {
+      //     console.log(`${userId} join the room`);
+      //     console.log(`num of students in the room: ${numStudentsInTheRoom}`);
+      //     setStudents(numStudentsInTheRoom);
+      //     console.log(`students value after updating: ${students}`);
+      // });
+
+      //   socket.on('someoneDisconnected', (userId) => {
+      //     console.log(`${userId} leave the room`);
+      // });
+
         return () => {
+          console.log('inside the client return')
           socket.off('updateContent');
+          socket.disconnect();
       };
-  },[]);
+  },[codeBlockName]);
 
   const handleEdit = (updatedContent) => { 
     setContent(updatedContent);
@@ -30,11 +64,10 @@ const EditorScreen = () => {
   };
 
   return (
-    <>
       <div className="editor-block"> 
       <Editor value={content} onChange={handleEdit} theme="vs-dark" defaultLanguage="javascript"/>;
+      <h3>Number of students in the room: {students}</h3>
       </div>
-    </>
     );
   }
 
