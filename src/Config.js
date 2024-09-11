00998cf05ab8e22bbad4eadea673667cc7b0824f
Yosef-Perelman <yosefper@gmail.com>
2024-09-11 19:44:28 +0300

    Edited the rooms names.

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,4 @@
 export const rooms = [
-    { name: 'Basic Example' },
-    { name: 'Advanced Example' },
-    { name: 'Tutorial Code' },
     { name: 'Basic Function Declaration' },
     { name: 'Variable Declaration' },
     { name: 'For Loop' },
